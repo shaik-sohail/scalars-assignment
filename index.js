bebae124f4cf6df670c9ad313ae237ee8a59432a
shaik-sohail <sohail.shaik@zemosolabs.com>
2022-06-30 10:42:32 +0530

    added resolvers and query files

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 const { ApolloServer, gql } = require("apollo-server");
 const { typeDefs } = require("./schema");
 const { movies } = require("./db");
-const resolvers = {
-  Query: {
-    movies: (parent, args, context) => {
-      return movies;
-    },
-    movie: (parent, args, context) => {
-      const index = movies.findIndex((movie) => movie.id === args.id);
-      return index !== -1 ? movies[index] : null;
-    },
-  },
-};
+const { resolvers } = require("./resolvers/Query");
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: {
+    movies,
+  },
 });
 server.listen().then(({ url }) => {
   console.log("Listening at : " + url);
